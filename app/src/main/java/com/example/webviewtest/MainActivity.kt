b8a56d6ff80ef581e5eeeac0fc1e1f34659f3f32
VmYs <ysfrank1@gmail.com>
2022-03-03 16:58:21 +0800

    add AudioRecord - 20220303

--- app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.webviewtest
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.webviewtest.databinding.ActivityMainBinding
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +29,54 @@ class MainActivity : AppCompatActivity() {
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.webViewClient = WebViewClient()
         binding.webView.loadUrl("https://www.bing.com")
+
+        if (ContextCompat.checkSelfPermission(this,
+                        android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.RECORD_AUDIO), 1)
+        } else {
+            val audioRecord = audioRecordInit()
+            audioRecord?.startRecording()
+            Log.d("record", "Recording has started")
+        }
+
     }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    audioRecordInit()
+                } else {
+                    Toast.makeText(this, "You denied this permission",
+                            Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun audioRecordInit(): AudioRecord? {
+        try {
+            return AudioRecord.Builder()
+                    .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+                    .setAudioFormat(AudioFormat.Builder()
+                            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                            .setSampleRate(32000)
+                            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                            .build())
+                    .build()
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+        return audioRecordInit()
+    }
+
+
+
 }
